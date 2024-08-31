da2c029c4f886120757aa78d5c5151f70285d0fb
adiarhmn <adiauliarahmn@gmail.com>
2024-08-31 18:00:13 +0800

    Upload Project

---
+++ database/migrations/2024_08_31_095020_create_table_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_users', function (Blueprint $table) {
+            $table->id('id_user');
+            $table->string('username');
+            $table->string('password');
+            $table->string('email');
+            $table->string('token');
+            $table->enum('level', ['admin', 'mitra', 'investor']);
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_users');
+    }
+};
