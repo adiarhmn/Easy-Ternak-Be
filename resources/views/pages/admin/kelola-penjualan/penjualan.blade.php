74b4999ec29642c5827ee3f80c856f1f412c4d9d
2201301080 Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2024-10-01 14:29:23 +0800

    view: modified card view in penjualan menu

--- resources/views/pages/admin/kelola-penjualan/penjualan.blade.php
+++ resources/views/pages/admin/kelola-penjualan/penjualan.blade.php
@@ -3,29 +3,34 @@
 @section('content')
     <div class="row">
         @for ($i = 1; $i < 5; $i++)
-            <div class="col-md-3 col-sm-6 mb-4">
-                <div class="card">
-                    <div class="card-content">
-                        <div class="card-body p-2">
+        <div class="col-md-3 col-sm-6 mb-4">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body p-3">
+                        <div class="position-relative">
                             <img class="card-img-bottom img-fluid rounded-2" src="/images/kambing2.jpg"
                                 alt="Card image cap" style="height: 10rem; object-fit: cover;">
-                            <div>
-                                <h5 class="fw-bold mb-0 pb-0 mt-1">Kambing Boer</h5>
-                            <p id='deskripsi' class="card-text mt-0 pt-0">
-                                {{ Str::limit('Usia 15 bulan Tinggi 82cm Berat 16 Kg Kambing Hitam', 50) }}
-                            </p>
-                                <h5 class="card-text mt-0 pt-0"><small>Tekad Jaya Farm</small></h5>
+                            <span class="badge bg-secondary position-absolute" style="bottom: 10px; right: 10px;">Kambing Boer</span>
+                        </div>
+                        <div>
                             <div class="d-flex justify-content-end">
-                                <div>
-                                    <a href="{{url('admin/penjualan/detail/1')}}" class="btn btn-secondary btn-sm">Detail</a>
-                                </div>
+                                <h5 class="card-text mt-0 pt-0 fw-bold"><small>Tekad Jaya Farm</small></h5>
                             </div>
+                            <small id='deskripsi' class="card-text mt-0 pt-0">
+                                {{ Str::limit('Usia 15 bulan Tinggi 82cm Berat 16 Kg Kambing Hitam', 50) }}
+                            </small>
+                            <div class="d-flex justify-content-end mt-2">
+                                <div>
+                                    <a href="{{url('admin/penjualan/detail/1')}}" class="btn btn-outline btn-secondary btn-sm">Detail</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        @endfor
+        </div>
+    @endfor
+    
     </div>
 
 @endsection
