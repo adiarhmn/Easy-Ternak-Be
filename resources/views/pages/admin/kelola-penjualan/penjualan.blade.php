2842ad75cb040516d1760c05c5a78fa62e67f5c3
2201301080 Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2024-10-01 14:22:16 +0800

    view: modified card view in penjualan menu

--- resources/views/pages/admin/kelola-penjualan/penjualan.blade.php
+++ resources/views/pages/admin/kelola-penjualan/penjualan.blade.php
@@ -3,21 +3,23 @@
 @section('content')
     <div class="row">
         @for ($i = 1; $i < 5; $i++)
-            <div class="col-md-4 col-sm-12 mb-4">
+            <div class="col-md-3 col-sm-6 mb-4">
                 <div class="card">
                     <div class="card-content">
-                        <img class="card-img-bottom img-fluid" src="/images/kambing2.jpg"
-                            alt="Card image cap" style="height: 20rem; object-fit: cover;">
-                        <div class="card-body">
-                            <h4 class="card-title">Boer</h4>
-                            <p id='deskripsi' class="card-text">
+                        <div class="card-body p-2">
+                            <img class="card-img-bottom img-fluid rounded-2" src="/images/kambing2.jpg"
+                                alt="Card image cap" style="height: 10rem; object-fit: cover;">
+                            <div>
+                                <h5 class="fw-bold mb-0 pb-0 mt-1">Kambing Boer</h5>
+                            <p id='deskripsi' class="card-text mt-0 pt-0">
                                 {{ Str::limit('Usia 15 bulan Tinggi 82cm Berat 16 Kg Kambing Hitam', 50) }}
                             </p>
-                            <h5 class="card-text"><small>Tekad Jaya Farm</small></h5>
+                                <h5 class="card-text mt-0 pt-0"><small>Tekad Jaya Farm</small></h5>
                             <div class="d-flex justify-content-end">
                                 <div>
                                     <a href="{{url('admin/penjualan/detail/1')}}" class="btn btn-secondary btn-sm">Detail</a>
                                 </div>
+                            </div>
                             </div>
                         </div>
                     </div>
