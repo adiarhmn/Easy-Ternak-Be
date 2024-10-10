c85d352c9b3a32ce66197f43690a65cd06d8fd93
2201301080 Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2024-10-10 11:29:09 +0800

    view: add navigation panel in slot invesment

--- resources/views/fragments/container-admin.blade.php
+++ resources/views/fragments/container-admin.blade.php
@@ -101,15 +101,15 @@
                         <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
                             <li class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none">
                                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
-                                    role="button" aria-expanded="false" aria-haspopup="true">
-                                    <i class="fa fa-search"></i>
-                                </a>
-                                <ul class="dropdown-menu dropdown-search animated fadeIn">
-                                    <form class="navbar-left navbar-form nav-search">
-                                        <div class="input-group">
-                                            <input type="text" placeholder="Search ..." class="form-control" />
-                                        </div>
-                                    </form>
+                                role="button" aria-expanded="false" aria-haspopup="true">
+                                <i class="fa fa-search"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-search animated fadeIn">
+                                <form class="navbar-left navbar-form nav-search">
+                                    <div class="input-group">
+                                        <input type="text" placeholder="Search ..." class="form-control" />
+                                    </div>
+                                </form>
                                 </ul>
                             </li>
 
@@ -198,13 +198,13 @@
                                             <div class="user-box">
                                                 <div class="avatar-lg">
                                                     <img src="/assets/img/profile.jpg" alt="image profile"
-                                                        class="avatar-img rounded" />
+                                                    class="avatar-img rounded" />
                                                 </div>
                                                 <div class="u-text">
                                                     <h4>Hizrian</h4>
                                                     <p class="text-muted">hello@example.com</p>
                                                     <a href="profile.html"
-                                                        class="btn btn-xs btn-secondary btn-sm">View Profile</a>
+                                                    class="btn btn-xs btn-secondary btn-sm">View Profile</a>
                                                 </div>
                                             </div>
                                         </li>
@@ -230,9 +230,24 @@
             {{-- Start Main --}}
             <div class="container">
                 <div class="page-inner">
-                    <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
+                    <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-0">
+                        <h4 class="fw-bold mb-3">{{ $page }}</h4>
+                        @if($page!="Beranda")
+                        <ul class="breadcrumbs mb-3">
+							<li class="nav-home">
+								<a href="/admin/beranda">
+									<i class="icon-home"></i>
+								</a>
+							</li>
+							<li class="separator">
+								<i class="icon-arrow-right"></i>
+							</li>
+							<li class="nav-item text-secondary">
+								<a href="#">Slot</a>
+							</li>
+						</ul>
+                        @endif
                         <div>
-                            <h3 class="fw-bold mb-3">{{ $page }}</h3>
                             @if (!empty($deskripsi))
                                 <h6 class="op-7 mb-2">{{ $deskripsi }}</h6>
                             @endif
--- resources/views/pages/admin/kelola-slot/slot.blade.php
+++ resources/views/pages/admin/kelola-slot/slot.blade.php
@@ -14,35 +14,86 @@
 </style>
 @endsection
 @section('content')
-    <div class="row mt-0">
-        @for ($i = 1; $i < 8; $i++)
-            <div class="col-md-3 col-sm-6 mb-4">
-                <div class="card m-0">
-                    <div class="card-content">
-                        <div class="card-body p-3">
-                            <div class="position-relative">
-                                <img class="card-img-bottom img-fluid rounded-3" src="/images/kambing2.jpg"
-                                    alt="Card image cap" style="height: 10rem; object-fit: cover;">
-                                <span class="badge bg-secondary position-absolute" style="bottom: 10px; left: 10px;">Kambing Boer</span>
-                                <small>
-                                    <span class="badge bg-danger position-absolute fw-bold" style="top: 10px; right: 10px;">1/4 terisi</span>
-                                </small>
-                            </div>
-                            <div>
-                                <div class="d-flex justify-content-end">
-                                    <h5 class="card-text mt-0 pt-0 fw-bold"><small>Tekad Jaya Farm</small></h5>
+<ul class="nav nav-tabs nav-line nav-color-secondary" id="line-tab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="line-home-tab" data-bs-toggle="pill" href="#line-home" role="tab" aria-controls="pills-home" aria-selected="true">Slot Tersedia</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="line-profile-tab" data-bs-toggle="pill" href="#line-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Slot Penuh</a>
+    </li>
+    
+</ul>
+<div class="tab-content mt-3 mb-3" id="line-tabContent">
+    <div class="tab-pane fade show active" id="line-home" role="tabpanel" aria-labelledby="line-home-tab">
+        <div class="row mt-0">
+            @for ($i = 1; $i < 8; $i++)
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card m-0">
+                        <div class="card-content">
+                            <div class="card-body p-3">
+                                <div class="position-relative">
+                                    <img class="card-img-bottom img-fluid rounded-3" src="/images/kambing2.jpg"
+                                        alt="Card image cap" style="height: 10rem; object-fit: cover;">
+                                    <span class="badge bg-secondary position-absolute" style="bottom: 10px; left: 10px;">Kambing Boer</span>
+                                    <small>
+                                        <span class="badge bg-danger position-absolute fw-bold" style="top: 10px; right: 10px;">1/4 terisi</span>
+                                    </small>
                                 </div>
-                                <small id='deskripsi' class="card-text mt-0 pt-0">
-                                    {{ Str::limit('Usia 15 bulan Tinggi 82cm Berat 16 Kg Kambing Hitam', 50) }}
-                                </small>
-                                <div class="d-flex justify-content-end mt-2">
-                                    <h5 class="card-text"><small style="font-size: 0.6em">Dibuat: 2023-07-09 15:34:00</small></h5>
+                                <div>
+                                    <div class="d-flex justify-content-end">
+                                        <h5 class="card-text mt-0 pt-0 fw-bold"><small>Tekad Jaya Farm</small></h5>
+                                    </div>
+                                    <small id='deskripsi' class="card-text mt-0 pt-0">
+                                        {{ Str::limit('Usia 15 bulan Tinggi 82cm Berat 16 Kg Kambing Hitam', 50) }}
+                                    </small>
+                                    <div class="d-flex justify-content-end mt-2">
+                                        <h5 class="card-text"><small style="font-size: 0.6em">Dibuat: 2023-07-09 15:34:00</small></h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endfor
+            @endfor
+        </div>
     </div>
+    <div class="tab-pane fade" id="line-profile" role="tabpanel" aria-labelledby="line-profile-tab">
+        <div class="row mt-0">
+            @if(true)
+            <p class="text-center mt-1">Tidak ada data slot penuh!</p>
+            @else
+            @for ($i = 1; $i < 8; $i++)
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card m-0">
+                        <div class="card-content">
+                            <div class="card-body p-3">
+                                <div class="position-relative">
+                                    <img class="card-img-bottom img-fluid rounded-3" src="/images/kambing2.jpg"
+                                        alt="Card image cap" style="height: 10rem; object-fit: cover;">
+                                    <span class="badge bg-secondary position-absolute" style="bottom: 10px; left: 10px;">Kambing Boer</span>
+                                    <small>
+                                        <span class="badge bg-danger position-absolute fw-bold" style="top: 10px; right: 10px;">1/4 terisi</span>
+                                    </small>
+                                </div>
+                                <div>
+                                    <div class="d-flex justify-content-end">
+                                        <h5 class="card-text mt-0 pt-0 fw-bold"><small>Tekad Jaya Farm</small></h5>
+                                    </div>
+                                    <small id='deskripsi' class="card-text mt-0 pt-0">
+                                        {{ Str::limit('Usia 15 bulan Tinggi 82cm Berat 16 Kg Kambing Hitam', 50) }}
+                                    </small>
+                                    <div class="d-flex justify-content-end mt-2">
+                                        <h5 class="card-text"><small style="font-size: 0.6em">Dibuat: 2023-07-09 15:34:00</small></h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+            @endif
+        </div>
+    </div>
+</div>
+    
 @endsection
