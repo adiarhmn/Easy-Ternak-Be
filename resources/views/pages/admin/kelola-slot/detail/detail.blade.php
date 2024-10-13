d6f087f88ac7632eac5c71cb67bca17ad862f05d
2201301080 Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2024-10-14 07:25:30 +0800

    view: modified detail slot view

--- resources/views/pages/admin/kelola-slot/detail/detail.blade.php
+++ resources/views/pages/admin/kelola-slot/detail/detail.blade.php
@@ -1,7 +1,7 @@
 @extends('fragments.container-admin')
 
 @section('content')
-    <div class="card">
+    <div class="card shadow-sm border-0">
         <div class="card-body">
             {{-- Topbar --}}
             @include('components.topbar.topbar-slot')
@@ -9,52 +9,77 @@
             <div class="tab-content mt-3 mb-3" id="line-tabContent">
                 <div class="tab-pane fade show active" role="tabpanel" aria-labelledby="line-home-tab">
                     <div class="card-body">
-                        <div class="row">
-                            <div class="col">
+                        <div class="row g-4">
+                            <div class="col-md-6">
                                 <div class="form-body">
-                                    <div class="form-group">
-                                        <label for="mitra">Mitra</label>
-                                        <input type="text" id="mitra" class="form-control" value="Nurependi"
-                                            name="mitra" readonly>
+                                    <div class="form-group mb-3">
+                                        <label for="mitra" class="form-label">Mitra</label>
+                                        <input type="text" id="mitra" class="form-control" value="Nurependi" name="mitra" readonly>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="jenis_investasi">Jenis Investasi</label>
-                                        <input type="text" id="jenis_investasi" class="form-control" value="Fattening"
-                                            name="jenis_investasi" readonly>
+                                    <div class="form-group mb-3">
+                                        <label for="jenis_investasi" class="form-label">Jenis Investasi</label>
+                                        <input type="text" id="jenis_investasi" class="form-control" value="Fattening" name="jenis_investasi" readonly>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="jenis_kambing">Jenis Kambing</label>
-                                        <input type="text" id="jenis_kambing" class="form-control" value="Boer"
-                                            name="jenis_kambing" readonly>
+                                    <div class="form-group mb-3">
+                                        <label for="jenis_kambing" class="form-label">Jenis Kambing</label>
+                                        <input type="text" id="jenis_kambing" class="form-control" value="Boer" name="jenis_kambing" readonly>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="keterangan">Keterangan</label>
-                                        <textarea class="form-control" id="keterangan" name="keterangan" rows="4" readonly>Usia 15 bulan Tinggi 82cm Berat 16 Kg</textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="gambar">Gambar</label>
-                                        <img src="/images/kambing2.jpg" alt="kambing2" id="gambar" class="form-control img-fluid"
-                                            style="height: 200px; object-fit: cover;">
+                                    <div class="form-group mb-3">
+                                        <label for="keterangan" class="form-label">Keterangan</label>
+                                        <textarea class="form-control" id="keterangan" name="keterangan" rows="4" readonly>Usia 15 bulan, Tinggi 82cm, Berat 16 Kg</textarea>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
+
+                            <div class="col-md-6">
                                 <div class="form-body">
-                                    <div class="form-group">
-                                        <label for="tanggal_beli">Tanggal Beli</label>
-                                        <input type="text" id="tanggal_beli" class="form-control" value="21 Februari 2022"
-                                            name="tanggal_beli" readonly>
+                                    {{-- Carousel Gambar --}}
+                                    <div id="carouselGambar" class="carousel slide mb-4" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img src="/images/kambing1.jpg" class="d-block w-100 rounded" alt="Kambing 1" style="height: 250px; object-fit: cover;">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h5>Gambar 1</h5>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="/images/kambing2.jpg" class="d-block w-100 rounded" alt="Kambing 2" style="height: 250px; object-fit: cover;">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h5>Gambar 2</h5>
+                                                </div>
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="/images/kambing3.jpg" class="d-block w-100 rounded" alt="Kambing 3" style="height: 250px; object-fit: cover;">
+                                                <div class="carousel-caption d-none d-md-block">
+                                                    <h5>Gambar 3</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselGambar" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselGambar" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="tanggal_jual">Tanggal Jual</label>
-                                        <input type="text" id="tanggal_jual" class="form-control" value="27 Februari 2022"
-                                            name="tanggal_jual" readonly>
+                                    {{-- Akhir Carousel Gambar --}}
+                                    <div class="form-group mb-3">
+                                        <label for="tanggal_beli" class="form-label">Tanggal Beli</label>
+                                        <input type="text" id="tanggal_beli" class="form-control" value="21 Februari 2022" name="tanggal_beli" readonly>
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label for="tanggal_jual" class="form-label">Tanggal Jual</label>
+                                        <input type="text" id="tanggal_jual" class="form-control" value="27 Februari 2022" name="tanggal_jual" readonly>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-actions d-flex justify-content-end grid gap-1">
-                            <a href="{{ url('admin/penjualan') }}" class="btn btn-secondary">Kembali</a>
+
+                        {{-- Tombol Aksi --}}
+                        <div class="form-actions d-flex justify-content-end mt-4">
+                            <a href="{{ url('admin/penjualan') }}" class="btn btn-secondary me-2">Kembali</a>
                         </div>
                     </div>
                 </div>
